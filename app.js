e5620f3f6cc944380017d326feb762f0912fb85b
Amy Simpson <simpson.r.amy@gmail.com>
2020-10-05 16:54:29 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express"),
+	app = express();
+
+app.set("view engine", "ejs");
+app.get("/", function(req,res){
+	res.render("home");
+});
+
+app.get("/about", function(req,res){
+	res.render("about");
+});
+
+
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+app.listen(port, host, function(){
+  console.log("Server started...");
+});
