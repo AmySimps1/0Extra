03a7bab8e0d2c2f324027cb1ee700719c5848ca5
AmySimps1 <simpson.r.amy@gmail.com>
2020-12-03 18:58:17 +0000

    app.js added along with the package.json etc.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express"),
 	app = express();
 
 app.set("view engine", "ejs");
+
 app.get("/", function(req,res){
 	res.render("home");
 });
@@ -10,10 +11,13 @@ app.get("/about", function(req,res){
 	res.render("about");
 });
 
+app.get("/thestars", function(req,res){
+	res.render("thestars");
+});
 
 const host = '0.0.0.0';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; 
 
 app.listen(port, host, function(){
-  console.log("Server started...");
+  console.log("Server started..." + port);
 });
