55f0f4f58e92e9b8e66b996ff723511de7b3618f
AmySimps1 <simpson.r.amy@gmail.com>
2020-12-03 19:22:47 +0000

    app styling

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.get("/thestars", function(req,res){
 });
 
 const host = '0.0.0.0';
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 
 app.listen(port, host, function(){
-  console.log("Server started..." + port);
+	console.log("Server started..." + port);
 });
